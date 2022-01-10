f3790ad2ffd911486f4b9263f911269a74be63d7
Jhonatan A <1548170+baguio@users.noreply.github.com>
2022-01-10 01:54:53 -0500

    Defined blog details with custom theme

---
+++ Sources/jhonatn-blog/blog.swift
@@ -0,0 +1,204 @@
+import Publish
+import Plot
+import Foundation
+import TokamakPublish
+
+struct Blog: Website {
+    enum SectionID: String, WebsiteSectionID {
+        case posts
+    }
+    
+    struct ItemMetadata: WebsiteItemMetadata {}
+    
+    var url = URL(string: "https://blog.jhona.tn")!
+    var name = "jhona.tn"
+    var description = "A tech experimentation blog"
+    var language: Language { .english }
+    var imagePath: Publish.Path? { nil }
+}
+
+let secondaryTextColor = Color(0x8A8A8A)
+let backgroundColor = Color(0xEEEEEE)
+
+extension Theme where Site == Blog {
+    static var blog: Self {
+        Theme(htmlFactory: BlogHTMLFactory())
+    }
+    
+    struct BlogHTMLFactory: TokamakHTMLFactory {
+        func makeIndexView(for index: Index, context: PublishingContext<Site>) throws -> some View {
+            Header(context: context)
+            VStack(alignment: .leading) {
+                Text(context.site.description)
+                    .font(.caption)
+                    .padding(.bottom)
+                ItemList(
+                    items: context.allItems(sortedBy: \.date, order: .descending),
+                    site: context.site
+                )
+            }
+            .frame(idealWidth: 820, maxWidth: 820)
+            .padding(.vertical, 40)
+            Footer(site: context.site)
+        }
+        
+        func makeSectionView(for _: Publish.Section<Site>, context: PublishingContext<Site>) throws -> some View {
+            EmptyView()
+        }
+        
+        func makeItemView(for item: Item<Site>, context: PublishingContext<Site>) throws -> some View {
+            Header(context: context)
+            VStack(alignment: .leading) {
+                item.body
+                //              Text("Tagged with: ")
+                TagList(item: item, site: context.site)
+            }
+            .frame(idealWidth: 820, maxWidth: 820)
+            .padding(.vertical, 40)
+            Footer(site: context.site)
+        }
+        
+        func makePageView(for page: Page, context: PublishingContext<Site>) throws -> some View {
+            Header(context: context)
+            VStack(alignment: .leading) {
+                page.body
+            }
+            .frame(idealWidth: 820, maxWidth: 820)
+            .padding(.vertical, 40)
+            Footer(site: context.site)
+        }
+        
+        func makeTagListView(for page: TagListPage, context: PublishingContext<Site>) throws -> some View {
+            Header(context: context)
+            VStack(alignment: .leading) {
+                Text("Browse all tags")
+                    .font(.headline)
+                HStack {
+                    ForEach(page.tags.sorted()) { tag in
+                        Tag(tag: tag, site: context.site)
+                    }
+                }
+            }
+            .frame(idealWidth: 820, maxWidth: 820)
+            .padding(.vertical, 40)
+            Footer(site: context.site)
+        }
+        
+        func makeTagDetailsView(for page: TagDetailsPage, context: PublishingContext<Site>) throws -> some View {
+            Header(context: context)
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Tagged with")
+                        .padding(.trailing)
+                    Tag(tag: page.tag, site: context.site)
+                }
+                .font(.headline)
+                Link(
+                    "Browse all tags",
+                    destination: URL(string: context.site.tagListPath.absoluteString, relativeTo: context.site.url)!
+                )
+                ItemList(
+                    items: context.items(
+                        taggedWith: page.tag,
+                        sortedBy: \.date,
+                        order: .descending
+                    ),
+                    site: context.site
+                )
+            }
+            .frame(idealWidth: 820, maxWidth: 820)
+            .padding(.vertical, 40)
+            Footer(site: context.site)
+        }
+    }
+    
+    
+    struct ItemList: View {
+        let items: [Item<Site>]
+        let site: Site
+        
+        var body: some View {
+            VStack(alignment: .leading) {
+                ForEach(items) { item in
+                    VStack(alignment: .leading) {
+                        Link(destination: URL(string: item.path.absoluteString, relativeTo: site.url)!) {
+                            Text(item.title)
+                                .font(.headline)
+                        }
+                        Text(item.description)
+                            .font(.caption)
+                    }
+                    .padding(20)
+                    .background(backgroundColor)
+                    .cornerRadius(8)
+                }
+            }
+        }
+    }
+    
+    struct TagList: View {
+        let item: Item<Site>
+        let site: Site
+        
+        var body: some View {
+            HStack(spacing: 5) {
+                ForEach(item.tags) { tag in
+                    Tag(tag: tag, site: site)
+                }
+            }
+        }
+    }
+    
+    struct Header: View {
+        let context: PublishingContext<Site>
+        
+        var body: some View {
+            Link(
+                context.site.name,
+                destination: URL(string: "/", relativeTo: context.site.url)!
+            )
+                .font(.system(size: 64, weight: .bold))
+                .foregroundColor(secondaryTextColor)
+                .padding(.horizontal, 40)
+                .padding(.vertical, 30)
+                .frame(minWidth: 0, maxWidth: .infinity)
+        }
+    }
+    
+    struct Footer: View {
+        let site: Site
+        var body: some View {
+            HStack {
+                Text("Generated using")
+                    .padding(0)
+                Link(destination: URL(string: "https://github.com/johnsundell/publish")!) {
+                    Text("Publish")
+                        .underline()
+                }
+                Text("and")
+                    .padding(0)
+                Link(destination: URL(string: "https://github.com/TokamakUI/Tokamak")!) {
+                    Text("Tokamak")
+                        .underline()
+                }
+            }
+            .foregroundColor(secondaryTextColor)
+        }
+    }
+    
+    struct Tag: View {
+        let tag: Publish.Tag
+        let site: Site
+        
+        var body: some View {
+            Link(destination: URL(string: site.path(for: tag).absoluteString, relativeTo: site.url)!) {
+                Text(tag.string)
+                    .padding(.horizontal, 6)
+                    .padding(.vertical, 4)
+                    .background(Color.black)
+                    .foregroundColor(.white)
+                    .cornerRadius(5)
+            }
+        }
+    }
+}
--- Sources/jhonatn-blog/main.swift
+++ Sources/jhonatn-blog/main.swift
@@ -4,3 +4,12 @@
 //
 //  Created by Jhonatan A. on 6/01/22.
 //
+
+import Publish
+
+try Blog().publish(
+    withTheme: .blog,
+    at: "docs",
+    rssFeedSections: [],
+    deployedUsing: .git("git@github.com:baguio/blog.git", branch: "generated")
+)
